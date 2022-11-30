8992ff9ed4c536cf2e02b3aadceeb81d7a2c8504
Nora Sandler <nlsandler@gmail.com>
2022-11-30 10:52:16 -0800

    chapter 20a: register allocation without coalescing

---
+++ lib/backward_dataflow.mli
@@ -0,0 +1,12 @@
+module Dataflow : functor (G : Cfg.CFG) -> sig
+  type 'var annotation = 'var Batteries.Set.t
+  type 'var annotated_block = 'var annotation G.basic_block
+  type 'var annotated_graph = 'var annotation G.t
+
+  val analyze :
+    ('var annotation G.t -> unit) ->
+    ('var annotated_graph -> 'var annotated_block -> 'var annotation) ->
+    ('var annotated_block -> 'var annotation -> 'var annotated_block) ->
+    unit G.t ->
+    'var annotated_graph
+end
--- lib/context.ml
+++ lib/context.ml
@@ -1,2 +1,3 @@
-(* Extra context about a TACKY function; used primarily for debugging *)
+(* Extra context about a TACKY or assembly function;
+ * used for debugging, looking things up in symbol table, etc *)
 type t = { filename : string; fun_name : string; params : string list }
